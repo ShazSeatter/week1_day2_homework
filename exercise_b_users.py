8f21aa76532ab02498c6f79661c5b1a9a6b542f5
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-24 20:00:38 +0000

    answered question 5. added 2 possible ways of it

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -74,6 +74,13 @@ print(users["Avril"]["pets"][0]["species"])
 
 # 5. Get the smallest of Erik's lottery numbers
 
+# erik_lotto = users["Erik"].get("lottery_numbers")
+
+# print(min(erik_lotto))
+
+print(min(users["Erik"].get("lottery_numbers")))
+
+# used min() method
 
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
