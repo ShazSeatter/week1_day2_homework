99b961a26a72c35a564bf4e27e4888715a16a03b
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-24 19:34:17 +0000

    answered question 3 in exercise b

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -59,7 +59,13 @@ users = {
 print(users["Jonathan"]["twitter"])
 
 # 2. Get Erik's hometown
+
+print(users["Erik"]["home_town"])
+
 # 3. Get the list of Erik's lottery numbers
+
+print(users["Erik"].get("lottery_numbers"))
+
 # 4. Get the species of Avril's pet Monty
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an list of Avril's lottery numbers that are even
