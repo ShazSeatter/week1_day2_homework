570ddd3df1fb4ed7943143d2510d777d4c173bca
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-24 20:36:00 +0000

    answered question 8

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -66,24 +66,52 @@ print(users["Erik"]["home_town"])
 
 print(users["Erik"].get("lottery_numbers"))
 
+# -------------------------------------------------- 
+
 # 4. Get the species of Avril's pet Monty
 
 print(users["Avril"]["pets"][0]["species"])
 
 # the dictionary is nested in a list, using indexes to access value at species 
 
+# -------------------------------------------------- 
 # 5. Get the smallest of Erik's lottery numbers
 
-# erik_lotto = users["Erik"].get("lottery_numbers")
+erik_lotto = users["Erik"].get("lottery_numbers")
 
-# print(min(erik_lotto))
+print(min(erik_lotto))
 
-print(min(users["Erik"].get("lottery_numbers")))
+# print(min(users["Erik"].get("lottery_numbers")))
 
 # used min() method
+# -------------------------------------------------- 
 
 # 6. Return an list of Avril's lottery numbers that are even
+
+# NEED TO RETURN BACK TO THIS !!!!!!! //// 
+
+# even_num = users["Avril"].get("lottery_numbers")
+
+# print(even_num)
+
+# for num in even_num:
+#   if num % 2 == 0:
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+erik_lotto.append(7)
+
+print(erik_lotto)
+
 # 8. Change Erik's hometown to Edinburgh
+
+users["Erik"]["home_town"] = "Edinburgh"
+
+print(users["Erik"]["home_town"])
+
+# meals["lunch"] = "fish & chips"
+# meals.get("lunch")
+
+
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
