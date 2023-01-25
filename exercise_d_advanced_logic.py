c04dc664eea7e2b7c7da3e57ab7e5a241de9a8bc
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-25 09:26:32 +0000

    saved file

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -25,10 +25,12 @@ print(difference)
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
    
 
+
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
 
 
 # 5. HARD! Print the sum of the numbers. 
